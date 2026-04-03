1b929b05d7f9045297ec82567b7a3b553ae778f3
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2026-04-03 19:51:59 +0600

    learning Java

---
+++ 004. Casting/Casting.java
@@ -0,0 +1,14 @@
+public class Casting {
+
+    public static void main(String[] args) {
+
+        double price = 100.00;
+        double finalPrice = price + 18.00;
+        System.out.println(finalPrice);
+
+        // casting
+        int p = 100;
+        int fp = p + (int) 18.50;
+        System.out.println(fp);
+    }
+}
