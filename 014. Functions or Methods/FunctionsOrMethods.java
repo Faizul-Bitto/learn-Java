11ba89fa71e6f2be0f84ed5420698e7e8a7f523a
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2026-04-05 16:04:55 +0600

    learning Java

--- 014. Functions or Methods/FunctionsOrMethods.java
+++ 014. Functions or Methods/FunctionsOrMethods.java
@@ -12,9 +12,17 @@ public class FunctionsOrMethods {
         System.out.println(a + b);
     }
 
+    public static int calculateSum(int c, int d) {
+        int summation = c + d;
+        return summation;
+    }
+
     public static void main(String[] args) {
         printJava();
         printName("John");
         summation(10, 10);
+
+        int result = calculateSum(5, 10);
+        System.out.println(result);
     }
 }
